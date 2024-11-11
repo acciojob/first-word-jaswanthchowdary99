22997af69b611b4874f22223df0570fc1a99493f
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-11 15:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	let trimmedStr = s.trim();
+	let firstSpaceStr = trimmedStr.indexOf(' ');
+	if(firstSpaceStr === -1){
+		return trimmedStr
+	}else{
+		return trimmedStr.slice(0, firstSpaceStr);
+	}
 }
 
 // Do not change the code below
